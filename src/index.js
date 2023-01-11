6f225056c1531a23214b0c80630eeb8a693a514f
cagatayyildiz <yildiz.cagatay@hotmail.com>
2023-01-11 15:31:43 +0300

    redux logger

--- src/index.js
+++ src/index.js
@@ -3,14 +3,16 @@ import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import { applyMiddleware, createStore } from 'redux';
+import logger from 'redux-logger';
 import thunk from 'redux-thunk';
 import App from './App';
 import reducer from './store/reducers'
 
 
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-const counterStore = createStore(reducer, applyMiddleware(thunk));
+const counterStore = createStore(reducer, applyMiddleware(thunk, logger));
 
 //combine reducer??
 
